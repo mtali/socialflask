6ffc202f9d2c5cbbc8f77545800fed561a41a710
mtali <emmanuelmtali@yahoo.com>
2019-03-31 00:14:06 +0300

    Add login view route and blank template

--- app/__init__.py
+++ app/__init__.py
@@ -19,5 +19,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
     return app
