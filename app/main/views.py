e5a0c6886d2834e5dcb030c3fc0e40810f9c526f
mtali <emmanuelmtali@yahoo.com>
2019-04-20 00:45:34 +0300

    Implement comment on posts

--- app/main/views.py
+++ app/main/views.py
@@ -12,16 +12,15 @@ from flask import (
 from flask_login import login_required, current_user
 
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
-from ..models import User, Role, Post
+from ..models import User, Role, Post, Comment
 from ..decorators import admin_required, permissions_required
 from ..permissions import SitePermission as Permission
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-
     form = PostForm()
     if current_user.can(Permission.WRITE_ARTICLE) and form.validate_on_submit():
         post = Post(body=form.body.data, author=current_user._get_current_object())
@@ -100,10 +99,32 @@ def edit_profile_admin(id):
     return render_template("admin/edit_profile.html", form=form, user=user)
 
 
-@main.route('/post/<int:id>')
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+@login_required
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('main/post.html', posts=[post])
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash('Your comment has been published.')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) / current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False
+    )
+    comments = pagination.items
+    return render_template('main/post.html',
+                           posts=[post],
+                           form=form,
+                           comments=comments,
+                           pagination=pagination)
 
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
@@ -197,15 +218,53 @@ def followed_by(username):
                            follows=follows)
 
 
-@main.route('/all')
+@main.route('/all/')
 @login_required
 def show_all():
     response = make_response(redirect(url_for('.index')))
     response.set_cookie("show_followed", '', max_age=30*24*60*60)
     return response
 
-@main.route('/followed')
+
+@main.route('/followed/')
 def show_followed():
     response = make_response(redirect(url_for('.index')))
     response.set_cookie("show_followed", '1', max_age=30*24*60*60)
     return response
+
+
+@main.route('/moderate/')
+@login_required
+@permissions_required(Permission.MODERATE_COMMENTS)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False
+    )
+    comments = pagination.items
+    return render_template('main/moderate.html',
+                           comments=comments,
+                           pagination=pagination,
+                           page=page)
+
+
+@main.route('/moderate/enable/<int:id>/')
+@login_required
+@permissions_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = False
+    db.session.add(comment)
+    return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
+
+
+@main.route('/moderate/disable/<int:id>/')
+@login_required
+@permissions_required(Permission.MODERATE_COMMENTS)
+def moderate_disable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = True
+    db.session.add(comment)
+    return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
